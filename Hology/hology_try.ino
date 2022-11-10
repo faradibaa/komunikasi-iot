85b0b220be5f54d46e1217ce8b1751d46f21ad7b
faradibaa <62602196+faradibaa@users.noreply.github.com>
2022-11-10 13:09:53 +0700

    Update hology_try.ino

--- Hology/hology_try.ino
+++ Hology/hology_try.ino
@@ -1,3 +1,5 @@
+// https://randomnerdtutorials.com/telegram-esp8266-nodemcu-motion-detection-arduino/
+
 #include <HCSR04.h> // HCSR04 ultrasonic sensor (by gamegine)
 #include <Servo.h>
 #include <ESP8266WiFi.h>
@@ -17,18 +19,18 @@ const *char ssid = "Nyanko";
 const *char pass = "meongnyan";
 
 // inisialisasi telegram bot
-#define token "5711045728:AAEnTxM37bzVPXFr-nCxALZuXShOHTWAH9c"
+#define TOKEN "5711045728:AAEnTxM37bzVPXFr-nCxALZuXShOHTWAH9c"
 
 // Use @myidbot to find out the chat ID of an individual or a group
 // Also note that you need to click "start" on a bot before it can
 // message you
-#define chat_id "1025656101"
+#define BOT_ID "1025656101"
 
 HCSR04 hc(5, 6); // trig pin, echo pin
 Servo servo;
 X509List cert(TELEGRAM_CERTIFICATE_ROOT);
 WiFiClientSecure client;
-UniversalTelegramBot bot(token, client);
+UniversalTelegramBot bot(TOKEN, client);
 
 void setup() {
   Serial.begin(9600);
@@ -39,8 +41,16 @@ void loop() {
   // mengkoneksikan ke WiFi
   Serial.print("Connecting Wifi: ");
   Serial.println(ssid);
-  WiFi.mode(WIFI_STA);
+  WiFi.mode(WIFI_STA); // ini buat apa ya?
   WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
   
   // Sensor ultrasonik
   distance = hc.dist();
@@ -48,7 +58,7 @@ void loop() {
   Serial.println(distance);
   if(distance < threshold) {
     // Chatbot telegram
-    // kirim notif telegram
+    bot.sendMessage(BOT_ID, "Bot started up", "");
     Serial.println("Notifikasi telah dikirim ke telegram!");
   }
 
